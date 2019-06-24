9d3dfa7cbd0f8519fbfc3336132971a11f57f377
wyjun <wyjunhui@126.com>
2019-06-24 15:29:25 +0800

    Create DubboProviderApplication.java

---
+++ dubbo-provider/src/main/java/com/springboot/provider/DubboProviderApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.provider;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DubboProviderApplication
+ * 服务提供启动类
+ */
+@EnableDubbo
+@SpringBootApplication
+public class DubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderApplication.class, args);
+    }
+}
